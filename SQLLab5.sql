cc4ba649f9d1636a5e4afb0fa92527c9f3e937a4
Crisnineball <119613966+CrisH2307@users.noreply.github.com>
2023-06-07 22:42:22 -0400

    Add files via upload

---
+++ SQLLab5.sql
@@ -0,0 +1,18 @@
+/* Employee */
+INSERT INTO Employee VALUES(1,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(2,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(3,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(4,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(5,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(6,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(7,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(8,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(9,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(10,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(11,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(12,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(13,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(14,'Mr.','Williamson','John','W','1964-05-21',3);
+INSERT INTO Employee VALUES(15,'Mr.','Williamson','John','W','1964-05-21',3);
+
+COMMIT;
